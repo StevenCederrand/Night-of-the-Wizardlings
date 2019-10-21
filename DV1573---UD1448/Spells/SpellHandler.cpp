afa9a019f6fea11531c869e063b81a112cbd6ae5
ludde01 <39077307+ludde01@users.noreply.github.com>
2019-10-21 15:30:05 +0200

    fix

--- DV1573---UD1448/Spells/SpellHandler.cpp
+++ DV1573---UD1448/Spells/SpellHandler.cpp
@@ -268,7 +268,7 @@ bool SpellHandler::specificSpellCollision(glm::vec3 spellPos, glm::vec3 playerPo
 { 
 	// sphereradius is wrong
 	bool collision = false;
-	float sphereRadius = 0.6f * scale * 4;
+	float sphereRadius = 1.0f * scale * 2;
 
 	glm::vec3 closestPoint = OBBclosestPoint(spellPos, axis, playerPos);
 	glm::vec3 v = closestPoint - spellPos;
