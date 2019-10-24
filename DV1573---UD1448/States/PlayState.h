98bba7f57fac03d87d77a67f9834e64e599a9b4e
Nicolas Chimrac <nico.black@hotmail.com>
2019-10-24 14:53:51 +0200

    working

--- DV1573---UD1448/States/PlayState.h
+++ DV1573---UD1448/States/PlayState.h
@@ -14,7 +14,6 @@
 #include <Renderer/HudObject.h>
 
 
-
 bool callbackFunc(btManifoldPoint& cp, const btCollisionObjectWrapper* obj1, int id1, int index1,
 	const btCollisionObjectWrapper* obj2, int id2, int index2);
 
