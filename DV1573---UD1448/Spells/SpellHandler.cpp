993c452465560ee79f34cf1dfb6aa53e2a6b5d60
Alexander Danliden <AlexanderAlsvold@hotmail.se>
2019-10-25 14:42:53 +0200

    dummy

--- DV1573---UD1448/Spells/SpellHandler.cpp
+++ DV1573---UD1448/Spells/SpellHandler.cpp
@@ -322,7 +322,7 @@ const uint64_t SpellHandler::getUniqueID()
 void SpellHandler::spellCollisionCheck()
 {
 	//get the list of att the players on the network
-	std::vector<NetworkPlayers::PlayerEntity> list = Client::getInstance()->getNetworkPlayersREF().getPlayersREF();
+	auto& list = Client::getInstance()->getNetworkPlayersREF().getPlayersREF();
 
 	for (size_t i = 0; i < list.size(); i++)
 	{
