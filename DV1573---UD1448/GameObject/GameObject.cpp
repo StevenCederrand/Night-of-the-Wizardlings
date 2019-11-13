42e365936ce4bf6d5984054cbfe5b4ebe539fe25
ludde01 <39077307+ludde01@users.noreply.github.com>
2019-11-13 14:33:11 +0100

    hitbox around character works

--- DV1573---UD1448/GameObject/GameObject.cpp
+++ DV1573---UD1448/GameObject/GameObject.cpp
@@ -91,7 +91,6 @@ void GameObject::loadMesh(std::string fileName)
 					AnimationMap::getInstance()->createAnimation(animationName, tempAnimation);
 					logTrace("Animation created: {0}", animationName);
 				}
-
 				tempMesh.addAnimation(animationName);
 			}
 
@@ -312,6 +311,7 @@ void GameObject::createRigidBody(CollisionObject shape, BulletPhysics* bp)
 
 	for (size_t i = 0; i < m_meshes.size(); i++)
 	{
+		
 		const std::vector<Vertex>& vertices = MeshMap::getInstance()->getMesh(m_meshes[i].name)->getVertices();
 
 		// Animated mesh case
