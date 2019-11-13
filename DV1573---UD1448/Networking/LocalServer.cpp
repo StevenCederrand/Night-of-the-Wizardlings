1eed8f711fecd3f9d7cca9a90c35ef2c302e9504
Alexander Danliden <AlexanderAlsvold@hotmail.se>
2019-11-13 14:14:47 +0100

    Fixed pickup points

--- DV1573---UD1448/Networking/LocalServer.cpp
+++ DV1573---UD1448/Networking/LocalServer.cpp
@@ -27,10 +27,6 @@ LocalServer::LocalServer()
 	m_timedPickupSpawner.setTotalExecutionTime(NetGlobals::roundTimeMS);
 	m_timedPickupSpawner.setExecutionInterval(NetGlobals::pickupSpawnIntervalMS);
 	m_timedPickupSpawner.registerCallback(std::bind(&LocalServer::spawnPickup, this));
-
-	unsigned int _time = unsigned int(time(NULL));
-	srand(_time);
-
 	m_pickupID = 0;
 }
 
@@ -117,6 +113,10 @@ void LocalServer::ThreadedUpdate()
 	uint32_t currentTimeMS = 0;
 	uint32_t lastTimeMS = 0;
 	uint32_t timeDiff = 0;
+
+	unsigned int _time = unsigned int(time(NULL));
+	srand(_time);
+
 	while (serverRunning) {
 		
 		lastTimeMS = currentTimeMS;
@@ -1054,22 +1054,22 @@ void LocalServer::createPickupSpawnLocations()
 	
 	PickupSpawnLocation spawn_one;
 	copyStringToCharArray(spawn_one.name, "Top");
-	spawn_one.position = glm::vec3(0.0f, 14.0f, 0.0f);
+	spawn_one.position = glm::vec3(10.0f, 14.0f, -1.0f);
 	m_pickupSpawnLocations.emplace_back(spawn_one);
 
 	PickupSpawnLocation spawn_two;
 	copyStringToCharArray(spawn_two.name, "Graveyard");
-	spawn_two.position = glm::vec3(70.0f, 3.0f, 0.67f);
+	spawn_two.position = glm::vec3(82.0f, 2.3f, 0.5f);
 	m_pickupSpawnLocations.emplace_back(spawn_two);
 
 	PickupSpawnLocation spawn_three;
 	copyStringToCharArray(spawn_three.name, "Maze");
-	spawn_three.position = glm::vec3(-94.4f, 3.0f, 2.0f);
+	spawn_three.position = glm::vec3(-84.4f, 2.6f, 0.08f);
 	m_pickupSpawnLocations.emplace_back(spawn_three);
 
 	PickupSpawnLocation spawn_four;
 	copyStringToCharArray(spawn_four.name, "Tunnels");
-	spawn_four.position = glm::vec3(0.28f, 3.0f, 54.0f);
+	spawn_four.position = glm::vec3(10.28f, 2.4f, 53.0f);
 	m_pickupSpawnLocations.emplace_back(spawn_four);
 
 }
