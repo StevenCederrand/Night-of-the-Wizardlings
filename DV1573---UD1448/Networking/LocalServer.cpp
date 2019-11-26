58556fa3af3431eaa43c49841566d641a9030597
Alexander Danliden <AlexanderAlsvold@hotmail.se>
2019-11-26 13:10:58 +0100

    Made damage buff omega OP but took away the extra health

--- DV1573---UD1448/Networking/LocalServer.cpp
+++ DV1573---UD1448/Networking/LocalServer.cpp
@@ -725,7 +725,7 @@ void LocalServer::handleCollisionWithSpells(HitPacket* hitpacket, SpellPacket* s
 		float damageMultiplier = 1.0f;
 
 		if (shooter->hasDamageBuff)
-			damageMultiplier = 2.5f;
+			damageMultiplier = 4.0f;
 
 		float totalDamage = hitpacket->damage * damageMultiplier;
 
@@ -888,8 +888,8 @@ void LocalServer::checkCollisionBetweenPlayersAndPickups()
 			if (isCollidingWithPickup(player, pickup)) {
 				// Give player buffs here
 				if (pickup.type == PickupType::HealthPotion) {
-					if(player.health < 100)
-						player.health = 100;
+					if(player.health < NetGlobals::PlayerMaxHealth)
+						player.health = NetGlobals::PlayerMaxHealth;
 					
 					RakNet::BitStream stream;
 					stream.Write((RakNet::MessageID)HEAL_BUFF);
@@ -902,7 +902,7 @@ void LocalServer::checkCollisionBetweenPlayersAndPickups()
 					player.hasDamageBuff = true;
 					RakNet::BitStream stream;
 					stream.Write((RakNet::MessageID)DAMAGE_BUFF_ACTIVE);
-					player.health = 200;
+					player.health = NetGlobals::PlayerMaxHealth;
 					player.Serialize(true, stream);
 					m_serverPeer->Send(&stream, HIGH_PRIORITY, RELIABLE_ORDERED_WITH_ACK_RECEIPT, 0, player.guid, false);
 
