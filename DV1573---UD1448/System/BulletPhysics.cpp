08d41aace21023cd090d4cbe33e0076cd05538e3
ludde01 <39077307+ludde01@users.noreply.github.com>
2019-11-29 14:36:45 +0100

    can walk up stairs

--- DV1573---UD1448/System/BulletPhysics.cpp
+++ DV1573---UD1448/System/BulletPhysics.cpp
@@ -163,10 +163,8 @@ btVector3 BulletPhysics::getCharacterSize() const
 btKinematicCharacterController* BulletPhysics::createCharacter(const glm::vec3& position, float& height)
 {
 	//create the character and add him to the dynamicsWorld
-
 	btScalar capsuleX = m_boxSize.getX()*0.8f;
 	btScalar capsuleY = m_boxSize.getY() * 2.0f;
-	//btScalar capsuleZ = m_boxSize.getZ() + 0.3f;
 	btScalar capsuleZ = m_boxSize.getZ()*0.9f;
 	//height of capsule is	totalHeight = height + radius * 2
 	//						height = totalHeight - radius * 2
@@ -186,7 +184,7 @@ btKinematicCharacterController* BulletPhysics::createCharacter(const glm::vec3& 
 	m_character = new btKinematicCharacterController(m_ghostObject, m_playerShape, 1.0f, btVector3(0.0f, 1.0f, 0.0f));
 	m_dynamicsWorld->addCollisionObject(m_ghostObject, btBroadphaseProxy::CharacterFilter, btBroadphaseProxy::StaticFilter | btBroadphaseProxy::DefaultFilter | NormalObjects | DestructableObjects);
 
-	//m_character->setStepHeight(10);
+
 	m_character->setMaxSlope(btRadians(80.0));
 	m_collisionShapes.push_back(m_playerShape);
 	m_dynamicsWorld->addAction(m_character);
